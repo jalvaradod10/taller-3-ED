76492c0eec4914f9d0975f5612e1bf0f21f7db32
Juan Alvarado UN <jalvaradod@uninorte.edu.co>
2025-09-03 11:58:31 -0500

    Actualizar Ventas.java

--- ManejoArchivos.java
+++ ManejoArchivos.java
@@ -1,27 +1,30 @@
-import java.io.*;
-import java.util.*;
+public class Venta {
+    private String id;
+    private String idCliente;
+    private String idProducto;
+    private int cantidad;
 
-public class ManejoArchivos {
-    public static List<String> leerArchivo(String nombreArchivo) {
-        List<String> lineas = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(nombreArchivo))) {
-            String linea;
-            while ((linea = br.readLine()) != null) {
-                lineas.add(linea);
-            }
-        } catch (IOException e) {
-            System.out.println("Error leyendo archivo: " + nombreArchivo);
-        }
-        return lineas;
+    public Venta(String id, String idCliente, String idProducto, int cantidad) {
+        this.id = id;
+        this.idCliente = idCliente;
+        this.idProducto = idProducto;
+        this.cantidad = cantidad;
     }
 
-    public static void escribirArchivo(String nombreArchivo, List<String> lineas) {
-        try (PrintWriter pw = new PrintWriter(new FileWriter(nombreArchivo))) {
-            for (String linea : lineas) {
-                pw.println(linea);
-            }
-        } catch (IOException e) {
-            System.out.println("Error escribiendo archivo: " + nombreArchivo);
-        }
+    @Override
+    public String toString() {
+        return id + "," + idCliente + "," + idProducto + "," + cantidad;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public String getIdProducto() {
+        return idProducto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
     }
 }
